e398b7c0766d33105d19e387a1349b75fd13a857
Abg Nazmi Syahirul <syahirul26@gmail.com>
2023-06-17 16:55:55 +0800

    initialized

---
+++ frontend/src/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Layout from "./components/layout/Layout";
+import Cashflow from "./pages/Cashflow";
+import Inventory from "./pages/Inventory";
+import Projections from "./pages/Projections";
+
+type Props = {};
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="cashflow" element={<Cashflow />} />
+      <Route path="inventory" element={<Inventory />} />
+      <Route path="projection" element={<Projections />} />
+    </Route>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
